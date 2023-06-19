87f32550348b1e543e63d3717a2229e81d0b992c
victor <victorapostol679@gmail.com>
2023-06-19 16:05:54 +0300

    removed server-side google-oauth

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -4,16 +4,13 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { IJwtPayload } from "../interfaces/jwt.interface";
 import { AuthService } from "../auth.service";
-import { Request } from "express";
 import { User } from "src/modules/users/entities/user.entity";
 
-@Injectable() // do from auth header
+@Injectable() 
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly cfg: ConfigService, private readonly authService: AuthService) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([(request: Request) => {
-        return request.cookies.auth;
-      }]),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: cfg.get<string>("JWT_SECRET")
     })
